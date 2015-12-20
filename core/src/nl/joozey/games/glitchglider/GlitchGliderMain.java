8bf54b9c6db2a6b935d38954cbf33fa2192d049a
Jos van Egmond <josvanegmond@Joss-MacBook-Pro-2.local>
2015-12-20 21:14:14 +0100

    Added ship

--- core/src/nl/joozey/games/glitchglider/GlitchGliderMain.java
+++ core/src/nl/joozey/games/glitchglider/GlitchGliderMain.java
@@ -1,27 +1,50 @@
 package nl.joozey.games.glitchglider;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
 
-public class GlitchGliderMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("assets/badlogic.jpg");
-	}
+import nl.joozey.games.glitchglider.entity.Ship;
+import nl.joozey.powerup.Asset;
+import nl.joozey.powerup.AssetService;
+import nl.joozey.powerup.GameMain;
+import nl.joozey.powerup.Log;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+public class GlitchGliderMain extends GameMain implements AssetService.OnLoadAssetCallback {
+
+    private AssetService _as;
+    private Ship _playerShip;
+
+    public void initialise() {
+        _as = AssetService.getInstance();
+        _as.loadAsset(this, Ship.class);
+    }
+
+    public void run() {
+        if(_playerShip != null) {
+            Matrix4 transform = _playerShip.getModelInstance().transform;
+            transform.rotate(0, 1, 0, 1);
+            setCamera(transform);
+        }
+    }
+
+    @Override
+    public void onAssetLoaded(Class<? extends Asset> assetClass) {
+        Log.d(assetClass.getName());
+        if (assetClass == Ship.class) {
+            _playerShip = _as.instantiateAsset(Ship.class);
+            setCamera(_playerShip.getModelInstance().transform);
+        }
+    }
+
+    private void setCamera(Matrix4 transform) {
+        Vector3 position = new Vector3(0, 1.2f, -1.7f);
+        position.add(transform.getTranslation(new Vector3()));
+
+        getCamera().up.set(0,1,0);
+        getCamera().direction.set(0,0,1);
+        getCamera().position.set(position);
+        getCamera().rotate(new Quaternion().setFromMatrix(transform).conjugate());
+        getCamera().update();
+    }
 }
---
+++ core/src/nl/joozey/games/glitchglider/entity/Ship.java
@@ -0,0 +1,14 @@
+package nl.joozey.games.glitchglider.entity;
+
+import nl.joozey.powerup.ModelAsset;
+
+/**
+ * Created by josvanegmond on 12/12/15.
+ */
+public class Ship extends ModelAsset {
+
+    @Override
+    public String getFileName() {
+        return "reconship.g3db";
+    }
+}
